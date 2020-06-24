b69c687bd6567f10a949112dd6ab572a4ca50b90
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-24 12:52:36 -0700

    added db scheme and functions

---
+++ db/Messages.js
@@ -0,0 +1,34 @@
+const db = require('./connection');
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+  username: Joi.string().alphanum().required(),
+  subject: Joi.string().required(),
+  message: Joi.string().max(500).required(),
+  imageUrl: Joi.string().uri({
+    scheme: [
+      /https?/
+    ]
+  })
+});
+
+const messages = db.get('messages');
+
+function getAll() {
+  return messages.find();
+}
+
+function insert(msg) {
+  const result = Joi.validate(msg, schema);
+  if (result.error === null) {
+    msg.created = new Date();
+    return messages.insert(msg);
+  } else {
+    return Promise.reject(result.error);
+  }
+}
+
+module.exports = {
+  getAll,
+  insert
+}
